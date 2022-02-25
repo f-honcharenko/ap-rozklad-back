9cda58dcaa1fc042dae343d44435ddaa09d9ede9
Filipp <akstoplay@gmail.com>
2022-02-25 01:16:29 +0100

    add /getSpecs/

--- src/services/timetables.ts
+++ src/services/timetables.ts
@@ -1,10 +1,23 @@
 import express from 'express';
-
+import rawData from '../data/output.json'
 export const timetable = express.Router();
 
 timetable.post('/ping', async (req, res, next) => {
     try {
         return next(Promise.resolve({ data:req.body.test[1], status:200 }));
+    }catch (error) {
+        return next(error);
+    } 
+});
+
+timetable.post('/getSpecs/', async (req, res, next) => {
+    try {
+        let responce = {lastUpdate:null};
+        Object.keys(rawData).forEach(element => {
+            responce[element] = Object.keys(rawData[element]);
+        });
+        delete responce.lastUpdate
+        return next(Promise.resolve({ data:responce, status:200 }));
     }catch (error) {
         return next(error);
     } 
