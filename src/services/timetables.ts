cce06d28968a64287b6e792ef73280686bed456c
Filipp <akstoplay@gmail.com>
2022-02-25 01:33:36 +0100

    add /getCal/

--- src/services/timetables.ts
+++ src/services/timetables.ts
@@ -3,14 +3,6 @@ import rawData from '../data/output.json'
 
 export const timetable = express.Router();
 
-timetable.post('/ping', async (req, res, next) => {
-    try {
-        return next(Promise.resolve({ data:req.body.test[1], status:200 }));
-    }catch (error) {
-        return next(error);
-    } 
-});
-
 timetable.post('/getSpecs/', async (req, res, next) => {
     try {
         let responce = {lastUpdate:null};
@@ -24,7 +16,6 @@ timetable.post('/getSpecs/', async (req, res, next) => {
     } 
 });
 
-
 timetable.post('/getInfo/', async (req, res, next) => {
     try {
         return next(Promise.resolve({ data:{
@@ -35,3 +26,31 @@ timetable.post('/getInfo/', async (req, res, next) => {
         return next(error);
     } 
 });
+
+timetable.post('/getCal/', async (req, res, next) => {
+    try {
+        let _temp = new Date(req.body.start);
+        _temp.setDate(_temp.getDate() - 3);
+        const _start = _temp.valueOf();
+        const _end = new Date(req.body.end).valueOf();
+        const _course = req.body.spec;
+        const _subGroup = req.body.group;
+        if (!_start || !_end || !_course || !_subGroup) { 
+            return next(Promise.resolve({ data: {msg:'Недостаточно входных параметров'}, status:400 }));
+        }
+        let _responce = [];
+        if (rawData[_course][_subGroup]) {
+            for (const date in rawData[_course][_subGroup]) {
+                if ((Number(date) > _start) && (Number(date) < _end)) {
+                    _responce.push(...rawData[_course][_subGroup][date])
+                }
+            }
+            return next(Promise.resolve({ data:_responce, status:200 }));
+        } else {
+            return next(Promise.resolve({ data:{msg:'По данному запросу ничего не найдено'}, status:404 }));
+        }
+    }catch (error) {
+        return next(error);
+    } 
+});
+
--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -3,6 +3,7 @@ export async function errorHandler(err, req, res, next) {
         const responce = await err;
         return res.status(responce.status).json(responce.data);
     } catch (error) {
+        console.log(error);
         return res.status(500).json({msg:'Непредвиденная ошибка сервера',err:error});
     }
 }
