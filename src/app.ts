11dd8c82d7b8f5f8d159a28c664fe73d4d22d0ed
Filipp <akstoplay@gmail.com>
2022-03-21 23:52:48 +0100

    test webhook

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,12 @@ import express from 'express';
 import compression from 'compression';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
 
-import swagger from './swagger'
 import routers from './routes';
-import swaggerFile from './docs/swagger-output.json'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// swagger();
 
 app.use(cors());
 app.use(compression());
@@ -19,7 +15,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.use('/doc/', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(routers)
 
 export async function startServer() {
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 
 import express from 'express';
-import timetable from './services/timetables';
+// import timetable from './services/timetables';
+import webhook from './services/webhook';
 import { errorHandler } from './utils/errorHandler';
 
 const routers = express.Router();
 
-routers.use('/timetables/3/',timetable)
+// routers.use('/timetables/3/',timetable)
+routers.use('/webhook',webhook)
 routers.use(errorHandler);
 
 export default routers;
