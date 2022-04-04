ad2cca7e058d981273585bea3498f9bc1e1726f4
Filipp <akstoplay@gmail.com>
2022-04-04 11:08:04 +0200

    Revert "Revert "remove trash""
    This reverts commit 7884be6eefe83f7675c7eb7ede4b6bb5b47889f4.

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,11 @@
 
 import express from 'express';
-// import timetable from './services/timetables';
-import webhook from './services/webhook';
+import timetable from './services/timetables';
 import { errorHandler } from './utils/errorHandler';
 
 const routers = express.Router();
 
-// routers.use('/timetables/3/',timetable)
-routers.use('/webhook',webhook)
+routers.use('/timetables/3/',timetable)
 routers.use(errorHandler);
 
 export default routers;
